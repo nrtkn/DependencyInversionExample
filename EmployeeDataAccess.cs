59c735b03d146f247cc15eca75f81947b75576b7
nrtkn <38586514+nrtkn@users.noreply.github.com>
2022-01-21 23:56:00 +0300

    Add files via upload

---
+++ EmployeeDataAccess.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyInversionPrinciple
+{
+    public class EmployeeDataAccess : IEmployeeDataAccess
+    {
+        public Employee GetEmployeeDetails(int id)
+        {
+            Employee emp = new Employee()
+            {
+                ID = id,
+                Name = "Pranaya",
+                Department = "IT",
+                Salary = 10000
+            };
+
+            return emp;
+        }
+    }
+}
